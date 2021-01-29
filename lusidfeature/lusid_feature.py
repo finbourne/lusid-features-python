b2722239667b034a4fdd2be33ade5ae40dab88e9
slacki123 <sebastian.lacki@finbourne.com>
2021-01-29 17:42:57 +0000

    SE-389: fix a bug where tests pass but they're false positives

--- lusidfeature/lusid_feature.py
+++ lusidfeature/lusid_feature.py
@@ -1,3 +1,7 @@
+import inspect
+from functools import wraps
+
+
 def lusid_feature(*feature_codes):
     if len(feature_codes) < 1:
         raise ValueError("lusid_feature error: Decorator requires at least some input.")
@@ -10,13 +14,15 @@ def lusid_feature(*feature_codes):
             raise ValueError("lusid_feature error: Some decorated methods have no value passed. "
                              "Please make sure each lusid_feature decorator has a value code passed.")
 
-    def wrap_method(decorated_method):
-        def get_decorated_method(*decorated_method_args):
-            return decorated_method
-        get_decorated_method.decorator_value = list(feature_codes)  # <-- store the feature
-        return get_decorated_method
+    def decorator(decorated_method):
+        if decorated_method is None:
+            return
 
-    return wrap_method
+        @wraps(decorated_method)
+        def wrapper(*method_args, **kwargs):
+            return decorated_method(*method_args, **kwargs)
 
+        wrapper.decorator_value = list(feature_codes)  # <-- store the feature
+        return wrapper
 
-
+    return decorator
--- tests/test_other_tests_not_affected_by_decorator.py
+++ tests/test_other_tests_not_affected_by_decorator.py
@@ -7,25 +7,44 @@ from lusidfeature.lusid_feature import lusid_feature
 
 class ClassWithTestMethodsUsingManyDecorators(unittest.TestCase):
 
+    # This setUp and tearDown ensures that all tests have been run
+    def setUp(self) -> None:
+        print(f"Setting up {self._testMethodName}")
+        # skip test_control_method
+        if self._testMethodName == "test_control_method":
+            self.should_be_true = True
+        else:
+            self.should_be_true = False
+
+    def tearDown(self) -> None:
+        print(f"Tearing down {self._testMethodName} \n")
+        self.assertTrue(self.should_be_true)
+
     def test_dummy_method_1(self):
-        self.assertTrue(True)
+        print(f"inside {self._testMethodName}")
+        self.should_be_true = True
+        self.assertTrue(self.should_be_true)
 
     @lusid_feature("F3")
-    @classmethod
     @parameterized.expand(
         [
             ("test1", 1),
             ("test2", 2)
         ]
     )
-    def test_dummy_method_2(cls, test1, test2):
-        cls.assertTrue(type(test1) == str)
-        cls.assertTrue(type(test2) == int)
+    def test_method_with_decorator_on_top(cls, test1, test2):
+        print(f"inside {cls._testMethodName}")
+        cls.should_be_true = True
+        cls.assertTrue(cls.should_be_true)
+        cls.assertEqual(type(test1), str)
+        cls.assertEqual(type(test2), int)
 
     @lusid_feature("F4")
     @unittest.skip
-    def test_control_method(self):
-        pass  # Empty for testing purposes
+    def test_that_skip_works_properly(self):
+        print(f"inside {self._testMethodName}")
+        self.should_be_true = True
+        self.assertTrue(self.should_be_true)
 
     @parameterized.expand(
         [
@@ -33,8 +52,16 @@ class ClassWithTestMethodsUsingManyDecorators(unittest.TestCase):
             ("test2", 2)
         ]
     )
-    @lusid_feature("F5")
-    def test_dummy_method_2(self, test1, test2):
-        self.assertTrue(type(test1) == str)
-        self.assertTrue(type(test2) == int)
+    @lusid_feature("F5")  # This should never happen though
+    def test_method_with_decorator_on_bottom(self, test1, test2):
+        print(f"inside {self._testMethodName}")
+        self.should_be_true = True
+        self.assertTrue(self.should_be_true)
+        self.assertEqual(type(test1), str)
+        self.assertEqual(type(test2), int)
 
+    @lusid_feature("F6")
+    def test_simple_test_passes(self):
+        print(f"inside {self._testMethodName}")
+        self.should_be_true = True
+        self.assertTrue(self.should_be_true)
